a467dcb6a2be69181fa2c4d20ebe6b907d45e065
Ike <dylanraspino264@gmail.com>
2023-09-07 10:52:42 +0000

    start flask

---
+++ stage-01-flask/main.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+
+@app.route("/api")
+def get_parameters():
+    param1 = request.args.get('slack_name')
+    param2 = request.args.get('param2')
+
+    if param1 is None or param2 is None:
+        return jsonify({"error": "Both parameters (slack_name and track) are supposed to be passed in"}), 400
+
+    response_data = {
+        "slack_name": param1,
+        "track": param2,
+    }
+
+    return jsonify(response_data)
+
+
+if __name__ == "__main__":
+    app.run()
