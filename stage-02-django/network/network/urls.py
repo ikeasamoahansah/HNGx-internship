3475593ea47674401233a8ee418dd19e413037bc
Ike <dylanraspino264@gmail.com>
2023-09-11 22:15:06 +0000

    postman command work

--- stage-02-django/network/network/urls.py
+++ stage-02-django/network/network/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from api.views import CustomAuthToken
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('api-auth/', include('rest_framework.urls')),
+    path('api-token-auth/', CustomAuthToken.as_view()),
 ]
